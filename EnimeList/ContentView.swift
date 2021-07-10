5197e5693242a740b3daebf1151e28e05dba0a26
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-07-10 18:56:11 +0700

    Initial Commit

---
+++ EnimeList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnimeList
+//
+//  Created by Farhandika Zahrir Mufti guenia on 10/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnimeList/EnimeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnimeListApp.swift
+//  EnimeList
+//
+//  Created by Farhandika Zahrir Mufti guenia on 10/07/21.
+//
+
+import SwiftUI
+
+@main
+struct EnimeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
